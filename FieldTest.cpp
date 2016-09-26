f70a2d8f36346ca85078eb0eeeb1697e8231a6ca
JT Abbott <johnabbott8481@gmail.com>
2016-09-26 11:52:09 -0700

    Made one test

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,8 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+TEST(FieldTest, isSafe)
+{
+	Field minefield;
+	ASSERT_FALSE(minefield.isSafe(16,16));
+}  
